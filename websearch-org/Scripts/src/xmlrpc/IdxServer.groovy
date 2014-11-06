032f24f1ada2c27dc95e1754d9e9476ac2c9276f
rsova <romansova@romans-MacBook-Pro.local>
2014-11-05 17:21:03 -1000

    deleted websearch

--- websearch-org/Scripts/src/xmlrpc/IdxServer.groovy
+++
@@ -1,8 +0,0 @@
-package xmlrpc
-import groovy.net.xmlrpc.*
-import java.net.ServerSocket
-//http://repository.codehaus.org/org/codehaus/groovy/groovy-xmlrpc/0.8/
-class IdxServer {
-//	def server = new XMLRPCServer()
-//	server.echo = {return it}
-}
